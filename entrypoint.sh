0f4e4a72d4c28111562399fbab63bdb4ad017655
Stefan Heitmüller <stefan.heitmueller@skaylink.com>
2025-11-27 12:56:24 +0100

    fix typo
    Signed-off-by: Stefan Heitmüller <stefan.heitmueller@skaylink.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Add config.php from custom location if set
 if [[ -n "${CUSTOM_CONFIG_PHP_LOCATION}" ]]; then
-    ln -sf "${CUSTOM_CONFIG_PHP_LOCATION}" /var/ww/html/include/config.php
+    ln -sf "${CUSTOM_CONFIG_PHP_LOCATION}" /var/www/html/include/config.php
 fi
 
 # Start cron service
